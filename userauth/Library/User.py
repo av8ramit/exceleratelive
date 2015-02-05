d024120a2b9d7e74b1d653c8bb99f2c6b73f9857
Neel <ibhadralobo@ucdavis.edu>
2015-02-05 02:03:54 -0800

    Added/Updated Quick View - Added Scroll Functionality

--- userauth/Library/User.py
+++ userauth/Library/User.py
@@ -34,25 +34,30 @@ class User(object):
         writing_scores = []
         reading_scores = []
         math_scores = []
-        # overall_score_difference = 0
-        # writing_score_difference = 0
-        # reading_score_difference = 0
-        # math_score_difference = 0
+        overall_score_difference = 0
+        writing_score_difference = 0
+        reading_score_difference = 0
+        math_score_difference = 0
         if len(self.tests_taken) >= 1:
 
             for test in self.tests_taken:
-               # s1.append([date_converter(test.date),test.score_summary.total_score()])
+                s1.append([date_converter(test.date),test.score_summary.total_score()])
                 writing_scores.append([date_converter(test.date), test.score_summary.section_scores[WRITING_TYPE]])
                 reading_scores.append([date_converter(test.date), test.score_summary.section_scores[READING_TYPE]])
                 math_scores.append([date_converter(test.date), test.score_summary.section_scores[MATH_TYPE]])
+            
                 
             if len(self.tests_taken) > 1:
-                return math_scores[-1][1], reading_scores[-1][1], writing_scores[-1][1] 
+                overall_score_difference = (s1[-1][1]) - (s1[0][1])
+                writing_score_difference = (writing_scores[-1][1]) - (writing_scores[0][1])
+                reading_score_difference = (reading_scores[-1][1]) - (reading_scores[0][1])
+                math_score_difference =  (math_scores[-1][1]) - (math_scores[0][1])
+                return math_scores[-1][1], reading_scores[-1][1], writing_scores[-1][1], s1[-1][1], math_score_difference, writing_score_difference, reading_score_difference, overall_score_difference 
             else:
-                return math_scores[0][1], reading_scores[0][1], writing_scores[0][1]
+                return math_scores[0][1], reading_scores[0][1], writing_scores[0][1], s1[0][1], 0, 0, 0, 0
 
         else: # no tests taken yet 
-            return 0,0,0
+            return 0,0,0,0,0,0,0,0
 
     def build(self):
         mkdir(user_directory(self.name, self.c))
